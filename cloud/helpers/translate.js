befef8bc673fdfb626d582170cf62bbfd4e6666a
pasinjunchusri <pasin.junchusri@gmail.com>
2017-04-03 01:56:53 +0700

    message

---
+++ cloud/helpers/translate.js
@@ -0,0 +1,8 @@
+'use strict';
+module.exports = function translate(i18n, string) {
+    let filename = __dirname + '/../../i18n/' + i18n + '.json';
+    //console.log(filename)
+    const lang = require('./loadJson')(filename);
+    //return lang;
+    return lang[string];
+}
